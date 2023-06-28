8ba68af8921df251f077d07eff40bfec78950dc1
Jean <jeanncavalcante@gmail.com>
2023-06-28 16:22:47 -0300

    feat: header

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="flex justify-between px-4 pt-4">
+      <h1>Dashboard</h1>
+      <h2>Seja bem vindo, Jean Cavalcante</h2>
+    </header>
+  );
+}
+
+export default Header;
